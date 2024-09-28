3b87a33c140ebfa6b09570eddf08789a5d133427
UJJWAL SHARMA <92508362+ujjwal0-2@users.noreply.github.com>
2024-09-28 17:04:37 +0530

    Create half-pyramid.cpp

---
+++ half-pyramid.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+
+{
+    int n;
+ cin >> n;
+ for(int i=0;i<n;i=i+1){
+     for(int j=0;j<i+1;j=j+1){
+         cout<<"* ";
+     }
+  cout<<endl;
+ }
+ 
+}
